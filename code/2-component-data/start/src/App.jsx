0ffcc14a301266caff4abb9ee656195251ab4662
Chris Harrison <chris.harrison@snagajob.com>
2021-04-19 23:03:13 -0400

    Reset starter file to match tutorial

--- code/2-component-data/start/src/App.jsx
+++ code/2-component-data/start/src/App.jsx
@@ -1,32 +1,19 @@
 import './index.css'
 import React from 'react';
 import RecipeTitle from './RecipeTitle'
-import IngredientList from './IngredientList'
+// TODO: Import IngredientList
+
 
 function App() {
     // TODO: Add recipe object
-    const recipe = {
-        title: 'Mashed potatoes',
-        feedback: {
-            rating: 4.8,
-            reviews: 20
-        },
-        ingredients: [
-            { name: '3 potatoes, cut into 1/2" pieces', prepared: false },
-            { name: '4 Tbsp butter', prepared: false },
-            { name: '1/8 cup heavy cream', prepared: false },
-            { name: 'Salt', prepared: true },
-            { name: 'Pepper', prepared: true },
-        ],
-    };
 
     return (
         <article>
             <h1>Recipe Manager</h1>
             {/* TODO: Add RecipeTitle component */}
-            <RecipeTitle title={recipe.title} feedback={recipe.feedback} />
+
             {/* TODO: Add IngredientList component */}
-            <IngredientList ingredients={recipe.ingredients} />
+
         </article>
     )
 }
