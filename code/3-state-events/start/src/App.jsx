4c11ab94d5962c31cb7ccfe8cba09a8989edbc1a
rohini-bali <rohini.bali@quantiphi.com>
2021-10-26 16:14:43 +0530

    add changes to handle state and events. recipe

--- code/3-state-events/start/src/App.jsx
+++ code/3-state-events/start/src/App.jsx
@@ -20,22 +20,35 @@ function App() {
     };
 
     // TODO: Create recipe state
+    const [ recipe, setRecipe ] = useState(initialRecipe);
 
     // TODO: Add new state property
+    const [ prepared, setPrepared ] = useState(false);
 
     // TODO: Create ingredientClick event listener
-
+    function ingredientClick(index) {
+    const updatedRecipe = { ... recipe };
+    updatedRecipe.ingredients[index].prepared = !updatedRecipe.ingredients[index].prepared;
+    setRecipe(updatedRecipe);
+}
     // TODO: Add the effect hook
+    useEffect(() => {
+        setPrepared(recipe.ingredients.every(i => i.prepared));
+    }, [recipe]);
 
     return (
         <article>
             <h1>Recipe Manager</h1>
 
             {/* TODO: Pass recipe metadata to RecipeTitle */}
+            <RecipeTitle title={recipe.title} feedback={recipe.feedback} />
+            
 
             {/* TODO: Pass ingredients and event listener to IngredientList */}
+            <IngredientList ingredients={recipe.ingredients} onClick={ ingredientClick } />
 
             {/* TODO: Add the prep work display */}
+            { prepared ? <h2>Prep work done!</h2> : <h2>Just keep chopping.</h2>}
         </article>
     )
 }
