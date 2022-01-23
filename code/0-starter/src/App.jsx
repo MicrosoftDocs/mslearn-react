002b255b8c2bea04fcdf1873f455ad062568b123
Prem Kumar <premraj259@outlook.com>
2022-01-24 03:24:01 +0530

    added RecipeTitle.jsx file
    and linked it in App.jsx

--- code/0-starter/src/App.jsx
+++ code/0-starter/src/App.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import RecipeTitle from './RecipeTitle'
 
 function App() {
     return (
         <article>
             <h1>Recipe Manager</h1>
+            <RecipeTitle />
         </article>
     )
 }
---
+++ code/0-starter/src/RecipeTitle.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function RecipeTitle() {
+    const title = 'Mashed potatoes';
+    return (
+        <h2>{ title }</h2>
+    )
+};
+export default RecipeTitle;
