4c38d211c007d336b61ee80446705e848a99501c
Prem Kumar <premraj259@outlook.com>
2022-01-24 03:17:50 +0530

    updated index.jsx with new components

--- code/0-starter/src/index.jsx
+++ code/0-starter/src/index.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './App';
 
 ReactDOM.render(
-    <h1>Hello, world!</h1>,
+    <App />,
     document.getElementById('app')
 );
